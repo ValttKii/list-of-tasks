a77dfb7cd808d71e7cff74a9d8ae62135b4ea3bb
heiskaleiska <jere.heinonen2@hotmail.fi>
2022-11-15 18:23:54 +0200

    Yö/Päivä- modet raakileena läpi Noin. 4.5h

--- app/src/main/java/com/example/tasklist/MainActivity.kt
+++ app/src/main/java/com/example/tasklist/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.example.tasklist
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CompoundButton
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switch_id = findViewById<Switch>(R.id.switch_id)
+
+        switch_id.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if(switch_id.isChecked()){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+        });
+
+
 
         val btnNext = findViewById<Button>(R.id.btnNext)
         btnNext.setOnClickListener {
