4a7aad7e2a237e8c7013df62b6116a8f2a9bcc0f
Joungi <korhonenj2@gmail.com>
2022-11-22 19:02:22 +0200

    hemmetin favorite

--- app/src/main/java/com/example/tasklist/FavoritesActivity.kt
+++ app/src/main/java/com/example/tasklist/FavoritesActivity.kt
@@ -2,12 +2,22 @@ package com.example.tasklist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.favorites_todo.*
+import kotlinx.android.synthetic.main.item_favorite.*
 
 class FavoritesActivity : AppCompatActivity() {
 
+    private lateinit var favoAdapter: FavAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.favorites_todo)
+        favoAdapter = FavAdapter(mutableListOf())
+
+        rvFavItems.adapter = favoAdapter
+        rvFavItems.layoutManager = LinearLayoutManager(this)
+
+
     }
 }
